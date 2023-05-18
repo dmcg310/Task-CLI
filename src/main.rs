da1694a73999d838cb7ad36ff3185d0485d86ebf
dmcg310 <120114728+dmcg310@users.noreply.github.com>
2023-05-18 18:36:40 +0100

    feat: read and write tasks to file

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,80 @@
 use std::env;
+use std::fs::{File, OpenOptions};
+use std::io::prelude::*;
+use std::io::{BufRead, BufReader};
 
 struct Task {
-    id: usize,
     description: String,
 }
 
-fn display_tasks(tasks: Vec<Task>) {
+fn build_task(description: String) -> Task {
+    Task { description }
+}
+
+fn display_tasks(tasks: Vec<String>) {
+    let mut task_id = 1;
+
     println!("Tasks");
     println!("-----");
 
     for task in tasks.iter() {
-        println!("{}. {}", task.id, task.description);
+        println!("{}. {}", task_id, task);
+        task_id += 1;
     }
 
     println!("-----");
 }
 
 fn add_task() {
-    let mut tasks: Vec<Task> = vec![];
     let args: Vec<String> = env::args().collect();
     let description = &args[2];
 
-    let task: Task = build_task(3, String::from(description));
-    tasks.push(task);
-    display_tasks(tasks);
+    let task: Task = build_task(String::from(description));
+    let mut file = OpenOptions::new()
+        .append(true)
+        .open("tasks.txt")
+        .expect("Unable to open file");
+
+    if let Err(err) = writeln!(file, "{}", task.description) {
+        eprintln!("Failed to write to file: {}", err);
+    } else {
+        println!("Task added successfully");
+    }
 }
 
-fn build_task(id: usize, description: String) -> Task {
-    Task { id, description }
+fn file_creation() {
+    let file_name = "tasks.txt";
+
+    if std::path::Path::new(&file_name).exists() {
+        println!("File exists")
+    } else {
+        match File::create(&file_name) {
+            Ok(_file) => {
+                println!("File created")
+            }
+            Err(err) => eprintln!("Failed to create file: {}", err),
+        }
+    }
+}
+
+fn tasks_from_file() -> Vec<String> {
+    let file = File::open("tasks.txt").expect("File not found.");
+    let buf = BufReader::new(file);
+    buf.lines()
+        .map(|x| x.expect("Could not parse line."))
+        .collect()
 }
 
 fn main() {
+    file_creation();
+
+    let tasks: Vec<String> = tasks_from_file();
     let args: Vec<String> = env::args().collect();
 
     // check if user wants to add task
     if &args.len() > &1 && &args[1] == "a" {
         add_task()
     } else {
-        // TODO: get tasks from a file
-        // boilerplate tasks for now
-        let mut tasks: Vec<Task> = vec![];
-        let task_1 = build_task(1, String::from("Clean"));
-        let task_2 = build_task(2, String::from("Walk"));
-
-        tasks.push(task_1);
-        tasks.push(task_2);
-
         display_tasks(tasks)
     }
 }
