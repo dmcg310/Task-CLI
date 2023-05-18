8814748740e9067d66f5d4f22441362facd9a138
dmcg310 <120114728+dmcg310@users.noreply.github.com>
2023-05-18 17:24:21 +0100

    feat: 'a' arg allows adding tasks

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,51 @@
 use std::env;
 
-fn get_tasks() {
-    let tasks: Vec<String> = env::args().collect();
+struct Task {
+    id: usize,
+    description: String,
+}
+
+fn display_tasks(tasks: Vec<Task>) {
+    println!("Tasks");
+    println!("-----");
+
+    for task in tasks.iter() {
+        println!("{}. {}", task.id, task.description);
+    }
+
+    println!("-----");
+}
+
+fn add_task() {
+    let mut tasks: Vec<Task> = vec![];
+    let args: Vec<String> = env::args().collect();
+    let description = &args[2];
+
+    let task: Task = build_task(3, String::from(description));
+    tasks.push(task);
     display_tasks(tasks);
 }
 
-fn display_tasks(tasks: Vec<String>) {
-    for task in tasks.iter().skip(1) {
-        println!("{}", task);
-    }
+fn build_task(id: usize, description: String) -> Task {
+    Task { id, description }
 }
 
 fn main() {
-    get_tasks();
+    let args: Vec<String> = env::args().collect();
+
+    // check if user wants to add task
+    if &args.len() > &1 && &args[1] == "a" {
+        add_task()
+    } else {
+        // TODO: get tasks from a file
+        // boilerplate tasks for now
+        let mut tasks: Vec<Task> = vec![];
+        let task_1 = build_task(1, String::from("Clean"));
+        let task_2 = build_task(2, String::from("Walk"));
+
+        tasks.push(task_1);
+        tasks.push(task_2);
+
+        display_tasks(tasks)
+    }
 }
