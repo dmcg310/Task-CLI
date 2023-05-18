83b47c2d32725588a14960524bcade40e455090e
dmcg310 <120114728+dmcg310@users.noreply.github.com>
2023-05-18 16:29:14 +0100

    feat: get tasks and display them

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::env;
+
+fn get_tasks() {
+    let tasks: Vec<String> = env::args().collect();
+    display_tasks(tasks);
+}
+
+fn display_tasks(tasks: Vec<String>) {
+    for task in tasks.iter().skip(1) {
+        println!("{}", task);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    get_tasks();
 }
